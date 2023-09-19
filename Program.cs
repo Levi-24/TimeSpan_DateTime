8731d5a3abe76adabe59c8fc368d0bbfa479fe0e
Levente Major <leventemajor36@gmail.com>
2023-09-19 11:59:42 +0200

    TImeSpan

--- Program.cs
+++ Program.cs
@@ -6,9 +6,19 @@ namespace ydffhgf
     {
         static void Main(string[] args)
         {
-            TimeSpan ts = new TimeSpan();
-            Console.WriteLine(ts);
+            //Eltelt idő tárolása
+            TimeSpan ts1 = new TimeSpan(100, 10, 20);
+            TimeSpan ts2 = new TimeSpan(03, 30, 22);
+            Console.WriteLine(ts1);
+            Console.WriteLine(ts2);
+            Console.WriteLine();
 
+            Console.WriteLine(3 * ts1);
+            Console.WriteLine(-ts1);
+            Console.WriteLine(ts1.Add(ts2));
+            Console.WriteLine(ts1.TotalHours);
+
+            //Dátum-idő tárolása
             DateTime dt = new DateTime();
             Console.WriteLine(dt);
 

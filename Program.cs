2db882fa883345c2832d052d873cfc0c207d1291
Levente Major <leventemajor36@gmail.com>
2023-09-19 12:08:28 +0200

    Date Time

--- Program.cs
+++ Program.cs
@@ -9,17 +9,22 @@ namespace ydffhgf
             //Eltelt idő tárolása
             TimeSpan ts1 = new TimeSpan(100, 10, 20);
             TimeSpan ts2 = new TimeSpan(03, 30, 22);
+            TimeSpan ts3 = TimeSpan.Parse("4.12:10:10.123");
+
             Console.WriteLine(ts1);
             Console.WriteLine(ts2);
+            Console.WriteLine(ts3);
             Console.WriteLine();
 
             Console.WriteLine(3 * ts1);
             Console.WriteLine(-ts1);
             Console.WriteLine(ts1.Add(ts2));
             Console.WriteLine(ts1.TotalHours);
-
+            Console.WriteLine(ts1.Minutes);
+            
+            Console.WriteLine();
             //Dátum-idő tárolása
-            DateTime dt = new DateTime();
+            DateTime dt = new DateTime(1990,10,21);
             Console.WriteLine(dt);
 
         }

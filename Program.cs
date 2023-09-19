096eb2ad0a40be797b294f8b43d740e9d25e7886
Levente Major <leventemajor36@gmail.com>
2023-09-19 11:49:15 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ydffhgf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TimeSpan ts = new TimeSpan();
+            Console.WriteLine(ts);
+
+            DateTime dt = new DateTime();
+            Console.WriteLine(dt);
+
+        }
+    }
+}
